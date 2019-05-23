85fccdd56abc8b379a706b1e743ad6bd10343a2d
oleksandr <oleksandrkravchukmail@gmail.com>
2019-05-23 14:08:45 -0400

    creating PagerAdapter in the VisitPagerActivity

--- app/src/main/java/com/example/root/vma/VisitPagerActivity.java
+++ app/src/main/java/com/example/root/vma/VisitPagerActivity.java
@@ -1,14 +1,44 @@
 package com.example.root.vma;
 
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewParent;
+
+import java.util.List;
 
 public class VisitPagerActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private List<Visit> mVisits;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visit_pager);
+
+
+        mViewPager = (ViewPager) findViewById(R.id.visit_view_pager);
+
+        mVisits = VisitLab.get(this).getVisits();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+               Visit visit = mVisits.get(position);
+               return VetFragment.newInstance(visit.getId());
+            }
+
+            @Override
+            public int getCount() {
+               return mVisits.size();
+            }
+        });
+
     }
 }
