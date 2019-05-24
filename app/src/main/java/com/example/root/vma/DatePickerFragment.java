24837d38e4174e86ffaa6c3df8c515e02fb2cae6
oleksandr <oleksandrkravchukmail@gmail.com>
2019-05-24 14:53:21 -0400

    Created newInstance method in the DatePickerFragment and add Bundle to it with date variable.

--- app/src/main/java/com/example/root/vma/DatePickerFragment.java
+++ app/src/main/java/com/example/root/vma/DatePickerFragment.java
@@ -6,8 +6,24 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Date;
 
 public class DatePickerFragment extends AppCompatDialogFragment {
+
+    public static final String ARG_DATE = "date";
+
+    private DatePicker mDatePicker;
+
+    public static DatePickerFragment newInstance (Date date){
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE,date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
