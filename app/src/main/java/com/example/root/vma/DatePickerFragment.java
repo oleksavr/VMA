2ce96d3ba1f7c2282b190fb5089f5db33f270d3c
oleksandr <oleksandrkravchukmail@gmail.com>
2019-05-23 21:45:44 -0400

    Set View as DateDialog in the DatePickerFragment

--- app/src/main/java/com/example/root/vma/DatePickerFragment.java
+++ app/src/main/java/com/example/root/vma/DatePickerFragment.java
@@ -4,12 +4,19 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class DatePickerFragment extends AppCompatDialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date,null);
+
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker)
                 .setPositiveButton(android.R.string.ok,null)
                 .create();
