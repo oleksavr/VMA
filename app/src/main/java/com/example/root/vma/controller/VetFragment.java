46438e046fef1155182bca200f04f7847e7aa8d9
oleksandr <oleksandrkravchukmail@gmail.com>
2019-05-29 20:06:26 -0400

    added implementation image capture in vet fragment

--- app/src/main/java/com/example/root/vma/controller/VetFragment.java
+++ app/src/main/java/com/example/root/vma/controller/VetFragment.java
@@ -3,15 +3,18 @@ package com.example.root.vma.controller;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.PatternMatcher;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.FileProvider;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -33,6 +36,7 @@ import com.example.root.vma.model.Visit;
 
 import java.io.File;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import static android.widget.CompoundButton.*;
@@ -42,8 +46,9 @@ public class VetFragment extends Fragment {
     public static final String ARG_VISIT_ID = "visit_id";
     public static final String DIALOG_DATE = "DialogDate";
 
-    public static final int REQUEST_DATE = 0;
-    public static final int REQUEST_CONTACT = 1;
+    private static final int REQUEST_DATE = 0;
+    private static final int REQUEST_CONTACT = 1;
+    private static final int REQUEST_PHOTO = 2;
 
 
     private Visit mVisit;
@@ -194,6 +199,29 @@ public class VetFragment extends Fragment {
         }
 
         mPhotoButton = (ImageButton) v.findViewById(R.id.pet_camera);
+        final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        boolean canTakePhoto = mPhotoFile != null && captureImage.resolveActivity(packageManager) != null;
+        mPhotoButton.setEnabled(canTakePhoto);
+
+        mPhotoButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Uri uri = FileProvider.getUriForFile(getActivity(),"com.example.root.vma.fileprovider",mPhotoFile);
+                captureImage.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+
+                List<ResolveInfo> cameraActivities = getActivity()
+                        .getPackageManager().queryIntentActivities(captureImage,PackageManager.MATCH_DEFAULT_ONLY);
+
+                for(ResolveInfo activity : cameraActivities){
+                    getActivity().grantUriPermission(activity.activityInfo.packageName,uri,Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                }
+
+                startActivityForResult(captureImage,REQUEST_PHOTO);
+
+            }
+        });
         mPhotoView = (ImageView) v.findViewById(R.id.pet_photo);
 
         return v;
