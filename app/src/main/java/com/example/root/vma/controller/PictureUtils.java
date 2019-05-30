67b57572546f17cdf1ad93533d80bac06984c9d2
oleksandr <oleksandrkravchukmail@gmail.com>
2019-05-30 18:26:14 -0400

    added another static method to the picture utilities class

--- app/src/main/java/com/example/root/vma/controller/PictureUtils.java
+++ app/src/main/java/com/example/root/vma/controller/PictureUtils.java
@@ -1,9 +1,20 @@
 package com.example.root.vma.controller;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
 
 public class PictureUtils {
+
+    public static Bitmap getScaledBitmap (String path, Activity activity){
+        Point size = new Point();
+        activity.getWindowManager().getDefaultDisplay().getSize(size);
+        return getScaledBitmap(path,size.x,size.y);
+    }
+
+
+
     public static Bitmap getScaledBitmap (String path, int destWidth, int destHeight){
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
