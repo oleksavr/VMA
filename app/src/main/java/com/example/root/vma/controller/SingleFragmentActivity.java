8d6aece6bf5f8ad681773fe3c0ca8eda5313ca92
oleksandr <oleksandrkravchukmail@gmail.com>
2019-06-01 20:51:45 -0400

    changed SingleFragmentActivity class to be able to use layout by its ID

--- app/src/main/java/com/example/root/vma/controller/SingleFragmentActivity.java
+++ app/src/main/java/com/example/root/vma/controller/SingleFragmentActivity.java
@@ -16,7 +16,7 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     @LayoutRes
     protected int getLayoutResId(){
-        return R.layout.activity_fragment;
+        return R.layout.activity_fragment_vet;
     }
 
 
