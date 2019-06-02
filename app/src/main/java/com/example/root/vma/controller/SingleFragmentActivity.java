2748c08933f5a135a22f4d567403eb256c10e51b
oleksandr <oleksandrkravchukmail@gmail.com>
2019-06-01 20:49:02 -0400

    changed SingleFragmentActivity class to be able to use layout by its ID

--- app/src/main/java/com/example/root/vma/controller/SingleFragmentActivity.java
+++ app/src/main/java/com/example/root/vma/controller/SingleFragmentActivity.java
@@ -2,6 +2,7 @@ package com.example.root.vma.controller;
 
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,10 +14,16 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
+    @LayoutRes
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment_vet);
+       setContentView(getLayoutResId());
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
